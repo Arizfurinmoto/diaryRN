651a008a42f8eb0e09bab8234a97791cb0e083ec
Arizfurinmoto <sz.godon@gmail.com>
2023-10-08 22:56:09 +0200

    background and button added

--- diaryRN/App.js
+++ diaryRN/App.js
@@ -1,20 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+    StyleSheet,
+    Text,
+    View,
+    ImageBackground,
+    StatusBar,
+} from "react-native";
+import paper from "./assets/paper.jpg";
+import AddEntryButton from "./components/AddEntryButton";
+import { useState } from "react";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Let's make this diary!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [modalVisible, setModalVisible] = useState(false);
+
+    const handleModal = () => {
+        console.log("Modal state changed to " + modalVisible);
+        setModalVisible(!modalVisible);
+    };
+
+    return (
+        <View style={styles.container}>
+            <StatusBar />
+            <ImageBackground
+                source={paper}
+                resizeMode='cover'
+                style={styles.backgroundImage}
+            >
+                <AddEntryButton handleModal={handleModal} />
+                <View style={styles.entriesContainer}>
+                    <Text>Let's make this diary!</Text>
+                </View>
+            </ImageBackground>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    backgroundImage: {
+        width: "100%",
+        height: "100%",
+        justifyContent: "flex-start",
+        alignItems: "center",
+    },
+    entriesContainer: {
+        padding: 15,
+    },
 });
---
+++ diaryRN/components/AddEntryButton.js
@@ -0,0 +1,28 @@
+import { Pressable, Text, StyleSheet } from "react-native";
+
+const AddEntryButton = (props) => {
+    return (
+        <Pressable style={styles.addEntryButton} onPress={props.handleModal}>
+            <Text style={styles.addEntryText}>NEW ENTRY</Text>
+        </Pressable>
+    );
+};
+
+export default AddEntryButton;
+
+const styles = StyleSheet.create({
+    addEntryButton: {
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "black",
+        padding: 10,
+        marginTop: 10,
+        width: "60%",
+        borderRadius: 14,
+        elevation: 10,
+    },
+    addEntryText: {
+        color: "white",
+        fontWeight: "bold"
+    },
+});
