e8a254495fa769af119472b4d8bdd12d81bac2d2
Arizfurinmoto <sz.godon@gmail.com>
2023-10-09 00:44:55 +0200

    DiaryEntry modal; adding new entry function; closing modal; getCurrentdate

--- diaryRN/App.js
+++ diaryRN/App.js
@@ -4,22 +4,37 @@ import {
     View,
     ImageBackground,
     StatusBar,
+    FlatList,
 } from "react-native";
+import { useState } from "react";
 import paper from "./assets/paper.jpg";
 import AddEntryButton from "./components/AddEntryButton";
-import { useState } from "react";
+import DiaryEntry from "./components/DiaryEntry.js";
 
 export default function App() {
     const [modalVisible, setModalVisible] = useState(false);
+    const [entriesList, setEntriesList] = useState([]);
 
     const handleModal = () => {
         console.log("Modal state changed to " + modalVisible);
         setModalVisible(!modalVisible);
     };
 
+    const updateEntriesList = (desc, curDate) => {
+        setEntriesList((currentEntry) => [
+            ...currentEntry,
+            { text: desc, date: curDate, id: Math.random().toString() },
+        ]);
+    };
+
     return (
         <View style={styles.container}>
             <StatusBar />
+            <DiaryEntry
+                visible={modalVisible}
+                handleModal={handleModal}
+                updateEntriesList={updateEntriesList}
+            />
             <ImageBackground
                 source={paper}
                 resizeMode='cover'
@@ -28,6 +43,23 @@ export default function App() {
                 <AddEntryButton handleModal={handleModal} />
                 <View style={styles.entriesContainer}>
                     <Text>Let's make this diary!</Text>
+                    <FlatList
+                        data={entriesList}
+                        renderItem={(itemData) => {
+                            itemData.index;
+                            return (
+                                <View
+                                    style={{ flexDirection: "row", padding: 5 }}
+                                >
+                                    <Text>{itemData.item.text}</Text>
+                                    <Text>{itemData.item.date}</Text>
+                                </View>
+                            );
+                        }}
+                        keyExtractor={(item, index) => {
+                            return item.id;
+                        }}
+                    />
                 </View>
             </ImageBackground>
         </View>
---
+++ diaryRN/components/DiaryEntry.js
@@ -0,0 +1,145 @@
+import { useState } from "react";
+import {
+    Button,
+    StyleSheet,
+    TextInput,
+    View,
+    Modal,
+    ImageBackground,
+    Text,
+} from "react-native";
+import paper from "../assets/paper.jpg";
+
+const DiaryEntry = (props) => {
+    const [enteredText, setEnteredText] = useState("");
+
+    const textInputHandler = (enteredText) => {
+        setEnteredText(enteredText);
+    };
+
+    const getCurrentDate = () => {
+        let day;
+        let month;
+        const date = new Date();
+        const d = date.getDate();
+        const m = date.getMonth() + 1;
+        const y = date.getFullYear();
+
+        if (d.toString().length < 2) {
+            day = "0" + d.toString();
+        } else {
+            day = d.toString();
+        }
+        if (m.toString().length < 2) {
+            month = "0" + m.toString();
+        } else {
+            month = m.toString();
+        }
+        return day + "." + month + "." + y.toString();
+    };
+
+    const currentDate = getCurrentDate();
+
+    const addEntryHandler = () => {
+        console.log("Entry added!");
+        props.updateEntriesList(enteredText, currentDate);
+        setEnteredText("");
+        props.handleModal();
+    };
+
+    const cancelHandler = () => {
+        console.log(getCurrentDate());
+        setEnteredText("");
+        props.handleModal();
+    };
+
+    const addPhotoHandler = () => {
+        console.log("Photo added!");
+    };
+
+    
+
+    return (
+        <Modal visible={props.visible} animationType='slide'>
+            <ImageBackground
+                source={paper}
+                resizeMode='cover'
+                style={styles.inputContainer}
+            >
+                <Text style={styles.textDate}>{currentDate}</Text>
+                <TextInput
+                    style={styles.textInput}
+                    placeholder={"What's up?"}
+                    onChangeText={textInputHandler}
+                    value={enteredText}
+                    multiline={true}
+                    textAlign='left'
+                    textAlignVertical='top'
+                />
+                <View style={styles.buttonContainer}>
+                    <View style={styles.button}>
+                        <Button
+                            title='Add Entry'
+                            onPress={addEntryHandler}
+                            color='black'
+                        />
+                    </View>
+                    <View style={styles.button}>
+                        <Button
+                            title='Add Photo'
+                            onPress={addPhotoHandler}
+                            color='black'
+                        />
+                    </View>
+                </View>
+                <View style={styles.buttonContainer}>
+                    <View style={styles.button}>
+                        <Button
+                            title='Cancel'
+                            onPress={cancelHandler}
+                            color='red'
+                        />
+                    </View>
+                </View>
+            </ImageBackground>
+        </Modal>
+    );
+};
+
+export default DiaryEntry;
+
+const styles = StyleSheet.create({
+    inputContainer: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        paddingTop: 20,
+    },
+    textInput: {
+        borderWidth: 1,
+        borderColor: "#bbc",
+        backgroundColor: "#fff",
+        color: "#000123",
+        borderRadius: 8,
+        width: "96%",
+        height: "75%",
+        padding: 15,
+    },
+    buttonContainer: {
+        flexDirection: "row",
+        marginVertical: 10,
+    },
+    button: {
+        width: "30%",
+        marginHorizontal: 10,
+    },
+    textDate: {
+        backgroundColor: "#fffddd",
+        width: "30%",
+        textAlign: "center",
+        padding: 5,
+        borderRadius: 5,
+        fontWeight: "bold",
+        marginBottom: 5,
+    },
+});
